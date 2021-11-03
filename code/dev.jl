76e17ce7169fb381ab6e22719181d2de0ede655b
Winant Pablo <pablo.winant@gmail.com>
2021-11-04 00:09:46 +0100

    Some edits.

--- code/dev.jl
+++ code/dev.jl
@@ -4,49 +4,101 @@ import Dolo: ergodic_distribution
 
 model = Model("code/consumption_savings.yaml")
 
-sol0 = time_iteration(model)
-grid = sol0.dr.grid_endo
+Dolo.set_calibration!(model; K=38)
+println("r: ", model.calibration.flat[:r])
+println("w: ", model.calibration.flat[:w])
 
-x = Dolo.nodes(Matrix, grid)[:]
-
-Dolo.set_calibration!(model, :K, 38)
-P, μ = ergodic_distribution(model, sol0)
-plot(μ)
+sol0 = time_iteration(model, (m,s)->min.(s, 1 .+0.01*(s.-1)), maxit=2)
 
 
+sol0 = improved_time_iteration(model, sol0.dr)
 
-pl = plot(x,x, label="K0")
+
+dr = sol0.dr
+
+
+dr([1.0])
+
+
+w = model.calibration.flat[:w]
+r = model.calibration.flat[:r]
+
+tab = tabulate(model, sol0.dr, :m)
+
+plot(tab[:m], tab[:m], ylim=(0,4), xlabel="\$m_t\$", ylabel="\$c(m_t)\$")
+plot!(tab[:m], tab[:c])
+plot!(tab[:m], (w .+tab[:m]*r)./(1+r), label="\$E(m_{t+1}) = m_t\$")
+
+
+
+
+N=1000
+T=1000
+sim = simulate(model, sol0.dr; N=N, s0=[2.5], T=T)
+pl = plot()
+for n=1:N
+    plot!(pl, sim[N=n,V=:m, T=1:40], label=nothing, color=:red, alpha=0.1)
+end
 pl
 
 
-@time begin
-    K0 = 50
+import StatsPlots
 
-    Dolo.set_calibration!(model, :K, K0)
-    sol = improved_time_iteration(model, sol0.dr)
-    P, μ = ergodic_distribution(model, sol.dr, sol.dr.grid_exo, sol.dr.grid_endo, sol0.dprocess)
+sim = simulate(model, sol0.dr; N=N, s0=[2.5], T=T)
 
-    K1 = x'*μ
+pl1 = StatsPlots.density(sim[T=1000, V=:m])
 
-    scatter!(pl, [K0], [K1])
+StatsPlots.density!(pl1, sim[N=1, V=:m, T=40:1000])
+
+
+
+
+μ = ergodic_distribution(model, sol0)
+
+plot(μ.axes[1].val, μ)
+
+
+
+
+
+
+function capital_supply(K0)
+    Dolo.set_calibration!(model; K = K0)
+    sol = improved_time_iteration(model, sol0.dr, verbose=false)
+    μ = ergodic_distribution(model, sol)
+    m = μ.axes[1].val
+    c = sol.dr(m[:,[CartesianIndex()]])
+    a = m .- c
+    return (;m,c,a)
+    K1 = a'*μ.data
+    return K1[1]
 end
 
+
+res = capital_supply(39)
+
+plot(res.m, res.m)
+plot!(res.m, res.m-res.c)
+
+
+K0vec = range(38, 41; length=100)
+
+@time K1vec = [capital_supply(k) for k in K0vec]
+
+plot(K0vec, K1vec)
+plot!(K0vec, K0vec)
 
 
 function fun(K0)
-    Dolo.set_calibration!(model, :K, K0)
-    sol = improved_time_iteration(model, sol0.dr, verbose=false)
-    grid = sol.dr.grid_endo
-    P, μ = ergodic_distribution(model, sol.dr, sol.dr.grid_exo, sol.dr.grid_endo, sol0.dprocess)
-
-    K1 = x'*μ
-    return K1-K0
+    K1 = capital_supply(K0)
+    return K1 - K0
 end
 
+fun()
 
 import Roots
 
-x0 = 48
+x0 = 30
 
 @time Roots.find_zero(fun, x0)
 
